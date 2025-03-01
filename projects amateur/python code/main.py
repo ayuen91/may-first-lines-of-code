3306d19c9be655a5e1bcf6998b5067c47db061f8
Ayuen91 <81303911+ayuen91@users.noreply.github.com>
2025-03-01 21:43:05 +0300

    made code readable

--- projects amateur/python code/main.py
+++ projects amateur/python code/main.py
@@ -1,5 +1,5 @@
-import datetime as dt
-now = dt.datetime.now()
+import datetime as date
+now = date.datetime.now()
 print('current time', now.strftime('%H:%M:%S, %Y:%m:%d'))
 
 #This is a simple python code that prints the current time and date.
