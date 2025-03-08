1ac5440419b1b49cb537c61521034f3b7319fd3d
ayuen <ayuenito@gmail.com>
2025-03-08 20:28:16 +0300

    fixed pyramidnum

--- python code/pyramidnum.py
+++ python code/pyramidnum.py
@@ -7,16 +7,14 @@ def diamond_num(number, rows):
         for i in range(rows - row -1):
             print(" ", end="")
         for j in range(2 * row + 1):
-            if number % 2 == 1:
-                print(number, end="")
+            print(number, end="")
             
         print()
     for row in range (rows - 1, 0, -1):
         for i in range(rows - row):
             print(" ", end="")
         for j in range(2 * row - 1):
-            if number % 2 == 1:
-                print(number, end="")
+            print(number, end="")
             
                                       
         print()
