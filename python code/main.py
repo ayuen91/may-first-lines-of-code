b47b2d496a21aa1e7c58dbea62cfe99c230d92d2
ayuen <ayuenito@gmail.com>
2025-03-10 17:33:39 +0300

    hello world

--- python code/main.py
+++ python code/main.py
@@ -1,5 +1,30 @@
-import datetime as date
-now = date.datetime.now()
-print('current time', now.strftime('%H:%M:%S, %Y:%m:%d'))
+menu = {"banana": 4,
+        "apple": 2,
+        "orange": 1.5, 
+        "pear": 3,
+        "grapes": 0.5,
+        "watermelon": 5,
+        "strawberry": 6,
+        "blueberry": 4,
+        "peach": 3,}
 
-#This is a simple python code that prints the current time and date.
+cart = []
+total = 0
+print('----------menu----------')
+for key, value in menu.items():
+    print(f"{key:10}: ${value:.2f}")
+print('------------------------')
+
+while True:
+    food = input("select food(q to quit): ").lower()
+    if food == 'q':
+            break
+    elif menu.get(food) is not None:
+            cart.append(food)
+print('-------YOUR ORDER--------')
+for food in cart:
+    total += menu.get(food)
+    print(food, end=' ')
+print()
+print(f"Total: ${total:.2f}")
+print('------------------------')
