1cc9879f88b29a8f8b3c603f04e614e066a69075
ayuen <ayuenito@gmail.com>
2025-03-13 14:18:17 +0300

    new code added
    made a lot of progresse today hope to make more in future

--- python projects/whileloop.py
+++
@@ -1,5 +0,0 @@
-food = input("What is your favorite food? ")
-while not food in ('q', 'quit', 'Quit', 'Q'):
-    print(f'Your favorite food is {food} ')
-    food = input("What is your other favorite food (press q to quit)? ")
-print('bye')
