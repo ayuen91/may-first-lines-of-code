cf6ad834fa5d17eb22dbda9171307fb2fed277a6
ayuen <ayuenito@gmail.com>
2025-03-14 14:21:45 +0300

    none

--- python projects/happybd.py
+++ python projects/happybd.py
@@ -1,9 +1,10 @@
 age = int(input("enter your age: "))
 name = input("enter your name: ")
-
+import time
 def print_birthday(name, age):
     for x in range(age):
-        for i in range(age - x - 1):
+        for i in range(1, age - 1):
             print(f" happy Birthday {name} you are {age} years old")
+            time.sleep(1)
     print()  
 print_birthday(name, age)      
