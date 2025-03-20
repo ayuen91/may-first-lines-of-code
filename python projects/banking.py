aebc04d805f2c613d6499f16f216c5f0a8ea113d
ayuen <ayuenito@gmail.com>
2025-03-20 16:32:50 +0300

    refactor banking system: streamline balance management and improve user interaction

--- python projects/banking.py
+++ python projects/banking.py
@@ -1,8 +1,10 @@
-def check_balance(balance):
+balance = 0
+
+def check_balance():
     print('***********************************')
     print(f'your current balance is ${balance:.2f}')
     print('***********************************')
-    return balance
+
 def deposit():
     amount = float(input("Enter amount to deposit: "))
     if amount < 0:
@@ -10,45 +12,49 @@ def deposit():
         return 0
     else:
         return amount
-def withdraw(balance):
+
+def withdraw():
+    global balance
     amount = float(input("Enter amount to withdraw: "))
     if amount > balance:
         print(f'insufficient funds. Your current balance is ${balance:.2f}')
+        return 0
     else:
         return amount
 
-balance = 0    
 def main():
-    
-    print('-----------------------------------')
-    print("Welcome to the banking system")
-    print("Please choose an option: ")
-    print('--------------------------------')
-    print("1. Check balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Quit")
-    print('-----------------------------------')
-    choice = float(input("Enter your choice: "))
-                    
-    if choice == 1:
-        check_balance(balance)
-    elif choice == 2:
-        amount = deposit()
-        balance += amount
-        print('***********************************')
-        print(f'your current balance is ${balance:.2f}')
-        print('***********************************')
-    elif choice == 3:
-        withdraw(amount)
-        balance -= amount
-        
-        
-    elif choice == 4:
-        print("Thank you for using the banking system")
-    else:
-        print("Invalid choice")
-    
-main()
+    global balance
+    is_running = True
+    while is_running:
+        print('-----------------------------------')
+        print("Welcome to the banking system")
+        print("Please choose an option: ")
+        print('--------------------------------')
+        print("1. Check balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Quit")
+        print('-----------------------------------')
+        choice = int(input("Enter your choice: "))
+                        
+        if choice == 1:
+            check_balance()
+        elif choice == 2:
+            amount = deposit()
+            balance += amount
+            check_balance()
+        elif choice == 3:
+            amount = withdraw()
+            balance -= amount
+            if amount > 0:
+                check_balance()
+        elif choice == 4:
+            is_running = False
+            print('--------------------------------------')
+            print("Thank you for using the banking system")
+            print('--------------------------------------')
+        else:
+            print("Invalid choice")
+
 if __name__ == '__main__':
     main()
